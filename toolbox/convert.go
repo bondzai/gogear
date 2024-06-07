f8341b28c3adaa89f58d30b5ce2fb4047ff37a0d
James Bond <introbond.dev@gmail.com>
2024-06-07 22:34:14 +0700

    Initial

---
+++ toolbox/convert.go
@@ -0,0 +1,19 @@
+package toolbox
+
+import "unicode"
+
+// CamelToSnakeCase converts camelCase strings to snake_case.
+func CamelToSnakeCase(s string) string {
+	var result []rune
+	for i, r := range s {
+		if unicode.IsUpper(r) {
+			if i > 0 {
+				result = append(result, '_')
+			}
+			result = append(result, unicode.ToLower(r))
+		} else {
+			result = append(result, r)
+		}
+	}
+	return string(result)
+}
